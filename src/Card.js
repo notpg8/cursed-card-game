dc0b898bea07dddcd5547750abf8a0ebf721b2b2
notpg8 <notpg8@gmail.com>
2022-01-25 10:50:46 +0100

    prettier

--- src/Card.js
+++ src/Card.js
@@ -1,85 +1,85 @@
 export class Card {
-  constructor(cardDetails) {
-    this.id = cardDetails.id;
-    this.rarity = cardDetails.rarity;
-    this.description = cardDetails.description;
-    this.stats = {
-      attack: this.assignRandomStatValue(this.rarity),
-      defense: this.assignRandomStatValue(this.rarity),
-      hp: this.assignRandomStatValue(this.rarity),
-    };
-  }
+	constructor(cardDetails) {
+		this.id = cardDetails.id
+		this.rarity = cardDetails.rarity
+		this.description = cardDetails.description
+		this.stats = {
+			attack: this.assignRandomStatValue(this.rarity),
+			defense: this.assignRandomStatValue(this.rarity),
+			hp: this.assignRandomStatValue(this.rarity),
+		}
+	}
 
-  // create card
-  get getCardStats() {
-    return `♥ ${this.stats.hp} | ⚔ ${this.stats.attack} | ❂ ${this.stats.defense}
-    `;
-  }
+	// create card
+	get getCardStats() {
+		return `♥ ${this.stats.hp} | ⚔ ${this.stats.attack} | ❂ ${this.stats.defense}
+    `
+	}
 
-  get getRarityIcon() {
-    switch (this.rarity) {
-      case "common":
-        return "●";
-      case "uncommon":
-        return "◆";
-      case "rare":
-        return "★";
-      default:
-        console.log(`RARITY ISSUE, NOT MATCHING ANY RARITY`);
-    }
-  }
+	get getRarityIcon() {
+		switch (this.rarity) {
+			case 'common':
+				return '●'
+			case 'uncommon':
+				return '◆'
+			case 'rare':
+				return '★'
+			default:
+				console.log(`RARITY ISSUE, NOT MATCHING ANY RARITY`)
+		}
+	}
 
-  get getRarityColor() {
-    switch (this.rarity) {
-      case "common":
-        return "gray";
-      case "uncommon":
-        return "#40677a";
-      case "rare":
-        return "gold";
-      default:
-        console.log(`RARITY ISSUE, NOT MATCHING ANY RARITY`);
-    }
-  }
+	get getRarityColor() {
+		switch (this.rarity) {
+			case 'common':
+				return 'gray'
+			case 'uncommon':
+				return '#40677a'
+			case 'rare':
+				return 'gold'
+			default:
+				console.log(`RARITY ISSUE, NOT MATCHING ANY RARITY`)
+		}
+	}
 
-  get getIsWeak() {
-    return this.stats.hp <= 20;
-  }
+	get getIsWeak() {
+		return this.stats.hp <= 20
+	}
 
-  get getRandomWeakText() {
-    switch (this.randomNumber(5)) {
-      case 1:
-        return "OOF";
-      case 2:
-        return "WHACK";
-      case 3:
-        return "SHIT!";
-      case 4:
-        return "CRAP";
-      case 5:
-        return "WEAK!";
-    }
-  }
+	get getRandomWeakText() {
+		switch (this.randomNumber(5)) {
+			case 1:
+				return 'OOF'
+			case 2:
+				return 'WHACK'
+			case 3:
+				return 'SHIT!'
+			case 4:
+				return 'CRAP'
+			case 5:
+				return 'WEAK!'
+		}
+	}
 
-  get getCardDescription() {
-    return this.description;
-  }
+	get getCardDescription() {
+		return this.description
+	}
 
-  assignRandomStatValue = (rarity) => {
-    if (rarity) {
-      if (rarity === "rare") {
-        return Math.floor(Math.random() * Math.floor(666)) + 1;
-      }
-      if (rarity === "uncommon") {
-        return Math.floor(Math.random() * Math.floor(120)) + 1;
-      }
-      if (rarity === "common") {
-        return Math.floor(Math.random() * Math.floor(60)) + 1;
-      }
-    }
-  };
+	assignRandomStatValue = (rarity) => {
+		if (rarity) {
+			if (rarity === 'rare') {
+				return Math.floor(Math.random() * Math.floor(666)) + 1
+			}
+			if (rarity === 'uncommon') {
+				return Math.floor(Math.random() * Math.floor(120)) + 1
+			}
+			if (rarity === 'common') {
+				return Math.floor(Math.random() * Math.floor(60)) + 1
+			}
+		}
+	}
 
-  randomNumber = (maxValue) => {
-    return Math.floor(Math.random() * Math.floor(maxValue)) + 1;
-  };
+	randomNumber = (maxValue) => {
+		return Math.floor(Math.random() * Math.floor(maxValue)) + 1
+	}
 }
--- src/index.js
+++ src/index.js
@@ -1,228 +1,230 @@
-import { Card } from "./Card";
-var socket = io();
+import { Card } from './Card'
+var socket = io()
 
-let cardsFromServerCopy = [];
+let cardsFromServerCopy = []
 
 const dealCards = () => {
-  renderCards(cardsFromServerCopy);
-  rotateCards();
-};
+	renderCards(cardsFromServerCopy)
+	rotateCards()
+}
 
 const requestNewCards = () => {
-  socket.emit("request-new-cards", (response)=>{
-  cardsFromServerCopy = []
-  cardsFromServerCopy.push(...response);
-  dealCards()
-  });
+	socket.emit('request-new-cards', (response) => {
+		cardsFromServerCopy = []
+		cardsFromServerCopy.push(...response)
+		dealCards()
+	})
 }
 
 // EXAMPLE OF SOCKET EMIT EVENT
 const emitFuckYouToServer = () => {
-  const messageValue = "fuck you";
-  return socket.emit("chat", messageValue);
-};
+	const messageValue = 'fuck you'
+	return socket.emit('chat', messageValue)
+}
 
-socket.on("deal-cards", (cardsFromServer) => {
-  cardsFromServerCopy.push(...cardsFromServer);
-  dealCards();
-});
+socket.on('deal-cards', (cardsFromServer) => {
+	cardsFromServerCopy.push(...cardsFromServer)
+	dealCards()
+})
 
 const returnRandomNumber = () => {
-  return Math.floor(Math.random() * Math.floor(5)) + 1;
-};
+	return Math.floor(Math.random() * Math.floor(5)) + 1
+}
 
 const randomColor = () => {
-  const red = Math.floor(Math.random() * Math.floor(257));
-  const green = Math.floor(Math.random() * Math.floor(257));
-  const blue = Math.floor(Math.random() * Math.floor(257));
-  const alpha = 1;
-  return `${red}, ${green}, ${blue}, ${alpha}`;
-};
+	const red = Math.floor(Math.random() * Math.floor(257))
+	const green = Math.floor(Math.random() * Math.floor(257))
+	const blue = Math.floor(Math.random() * Math.floor(257))
+	const alpha = 1
+	return `${red}, ${green}, ${blue}, ${alpha}`
+}
 
 const zoomOnCard = (e) => {
-  const cardStyle = e.target.style;
-  const allCards = document.querySelectorAll(".card");
-  allCards.forEach((card) => {
-    card.style.scale = 1;
-    card.style.zIndex = 0;
-    card.style.position = "relative";
-  });
-  rotateCards();
+	const cardStyle = e.target.style
+	const allCards = document.querySelectorAll('.card')
+	allCards.forEach((card) => {
+		card.style.scale = 1
+		card.style.zIndex = 0
+		card.style.position = 'relative'
+	})
+	rotateCards()
 
-  cardStyle.transform = "scale(1.9)";
-  cardStyle.zIndex = 10;
-  e.target.firstChild.style.visibility = "visible";
+	cardStyle.transform = 'scale(1.9)'
+	cardStyle.zIndex = 10
+	e.target.firstChild.style.visibility = 'visible'
 
-  e.target.querySelector(".notification-area") && statNotify(e);
+	e.target.querySelector('.notification-area') && statNotify(e)
 
-  emitFuckYouToServer();
-};
+	emitFuckYouToServer()
+}
 
 const createRarity = (currentCard, cardDiv) => {
-  const cardRarity = document.createElement("P");
-  cardRarity.classList.add("card-rarity");
-  cardRarity.appendChild(document.createTextNode(currentCard.getRarityIcon));
-  cardDiv.appendChild(cardRarity);
-};
+	const cardRarity = document.createElement('P')
+	cardRarity.classList.add('card-rarity')
+	cardRarity.appendChild(document.createTextNode(currentCard.getRarityIcon))
+	cardDiv.appendChild(cardRarity)
+}
 
 const createStats = (currentCard, cardDiv) => {
-  const cardStats = document.createElement("P");
-  cardStats.classList.add("card-stats");
-  cardStats.appendChild(document.createTextNode(currentCard.getCardStats));
-  cardDiv.appendChild(cardStats);
-  cardStats.setAttribute(
-    "style",
-    `${currentCard.rarity !== "rare" && "background-color: gold;"}`
-  );
-  if(currentCard.rarity === "rare"){
-    cardStats.setAttribute(
-      "style", "color: gold; text-shadow: 0 0 2px black"
-    );
-  }
-};
+	const cardStats = document.createElement('P')
+	cardStats.classList.add('card-stats')
+	cardStats.appendChild(document.createTextNode(currentCard.getCardStats))
+	cardDiv.appendChild(cardStats)
+	cardStats.setAttribute(
+		'style',
+		`${currentCard.rarity !== 'rare' && 'background-color: gold;'}`
+	)
+	if (currentCard.rarity === 'rare') {
+		cardStats.setAttribute('style', 'color: gold; text-shadow: 0 0 2px black')
+	}
+}
 
 const createNotificationArea = (currentCard, cardDiv) => {
-  const cardNotificationArea = document.createElement("span");
-  cardNotificationArea.innerHTML = currentCard.getRandomWeakText;
-  cardNotificationArea.setAttribute("style", `opacity: 0;`);
-  cardNotificationArea.classList.add("notification-area");
-  cardDiv.appendChild(cardNotificationArea);
-};
+	const cardNotificationArea = document.createElement('span')
+	cardNotificationArea.innerHTML = currentCard.getRandomWeakText
+	cardNotificationArea.setAttribute('style', `opacity: 0;`)
+	cardNotificationArea.classList.add('notification-area')
+	cardDiv.appendChild(cardNotificationArea)
+}
 
 const renderCards = (cards) => {
-  const playArea = document.querySelector(".play-area");
+	const playArea = document.querySelector('.play-area')
 
-  cards.map((card) => {
-    const initiatedCard = new Card(card);
-    const goldGradientCSS =
-      ", linear-gradient(to right, #BF953F, #FCF6BA, #FBF5B7, #AA771C)";
-    const cardDiv = document.createElement("DIV");
-    cardDiv.classList.add("card", initiatedCard.rarity);
-    cardDiv.setAttribute(
-      "style",
-      `background-size: 100%; 
+	cards.map((card) => {
+		const initiatedCard = new Card(card)
+		const goldGradientCSS =
+			', linear-gradient(to right, #BF953F, #FCF6BA, #FBF5B7, #AA771C)'
+		const cardDiv = document.createElement('DIV')
+		cardDiv.classList.add('card', initiatedCard.rarity)
+		cardDiv.setAttribute(
+			'style',
+			`background-size: 100%; 
       background-image: url("./media/cards-pngs-optimized/medium/${initiatedCard.id.toString()}.png");
       background-image: url("./media/cards-pngs-optimized/medium/${initiatedCard.id.toString()}.png")${
-        initiatedCard.rarity === "rare" && goldGradientCSS
-      };
+				initiatedCard.rarity === 'rare' && goldGradientCSS
+			};
       background-color: ${
-        initiatedCard.getRarityColor
-      }; background-size: cover;`
-    );
+				initiatedCard.getRarityColor
+			}; background-size: cover;`
+		)
 
-    // add card to the play area in the dom
-    playArea.appendChild(cardDiv);
+		// add card to the play area in the dom
+		playArea.appendChild(cardDiv)
 
-    // Create additional element of card in dom
-    createRarity(initiatedCard, cardDiv);
-    if (initiatedCard.getIsWeak) {
-      createNotificationArea(initiatedCard, cardDiv);
-    }
-    createStats(initiatedCard, cardDiv);
+		// Create additional element of card in dom
+		createRarity(initiatedCard, cardDiv)
+		if (initiatedCard.getIsWeak) {
+			createNotificationArea(initiatedCard, cardDiv)
+		}
+		createStats(initiatedCard, cardDiv)
 
-    cardDiv.addEventListener("click", function () {
-      revealCardDescription(initiatedCard);
-    });
-  });
+		cardDiv.addEventListener('click', function () {
+			revealCardDescription(initiatedCard)
+		})
+	})
 
-  disableDealButton();
-  enableDeleteButton();
+	disableDealButton()
+	enableDeleteButton()
 
-  document.querySelectorAll(".card").forEach((card) => {
-    card.addEventListener("click", zoomOnCard);
-  });
-};
+	document.querySelectorAll('.card').forEach((card) => {
+		card.addEventListener('click', zoomOnCard)
+	})
+}
 
 const disableDealButton = () => {
-  document.querySelector(".populate-cards").setAttribute("disabled", "true");
-};
+	document.querySelector('.populate-cards').setAttribute('disabled', 'true')
+}
 
 const enableDeleteButton = () => {
-  document.querySelector(".delete-cards").removeAttribute("disabled");
-};
+	document.querySelector('.delete-cards').removeAttribute('disabled')
+}
 
 const rotateCards = () => {
-  const cardsCommon = document.querySelectorAll(".common");
-  const cardsUncommon = document.querySelectorAll(".uncommon");
-  const cardsRare = document.querySelectorAll(".rare");
-  const cardsBack = document.querySelectorAll(".back");
+	const cardsCommon = document.querySelectorAll('.common')
+	const cardsUncommon = document.querySelectorAll('.uncommon')
+	const cardsRare = document.querySelectorAll('.rare')
+	const cardsBack = document.querySelectorAll('.back')
 
-  const applyRotationStyle = (cardsToRotate) => {
-    cardsToRotate.forEach((card) => {
-      const plusOrMinus = Math.random() < 0.5 ? -1 : 1;
-      const degrees =
-        (Math.floor(Math.random() * Math.floor(2.5)) + 1) * plusOrMinus;
-      card.style.transform = `rotate(${degrees}deg)`;
-    });
-  };
-  applyRotationStyle(cardsCommon);
-  applyRotationStyle(cardsUncommon);
-  applyRotationStyle(cardsRare);
-  applyRotationStyle(cardsBack);
-};
+	const applyRotationStyle = (cardsToRotate) => {
+		cardsToRotate.forEach((card) => {
+			const plusOrMinus = Math.random() < 0.5 ? -1 : 1
+			const degrees =
+				(Math.floor(Math.random() * Math.floor(2.5)) + 1) * plusOrMinus
+			card.style.transform = `rotate(${degrees}deg)`
+		})
+	}
+	applyRotationStyle(cardsCommon)
+	applyRotationStyle(cardsUncommon)
+	applyRotationStyle(cardsRare)
+	applyRotationStyle(cardsBack)
+}
 
 const deleteCards = () => {
-  const allCardsParent = document.querySelector(".play-area");
-  while (allCardsParent.firstChild) {
-    allCardsParent.removeChild(allCardsParent.firstChild);
-  }
-  document.querySelector(".populate-cards").removeAttribute("disabled");
-  document.querySelector(".delete-cards").setAttribute("disabled", "true");
-  clearInterface();
-};
+	const allCardsParent = document.querySelector('.play-area')
+	while (allCardsParent.firstChild) {
+		allCardsParent.removeChild(allCardsParent.firstChild)
+	}
+	document.querySelector('.populate-cards').removeAttribute('disabled')
+	document.querySelector('.delete-cards').setAttribute('disabled', 'true')
+	clearInterface()
+}
 
-document.querySelector(".populate-cards").addEventListener("click", requestNewCards);
-document.querySelector(".delete-cards").addEventListener("click", deleteCards);
+document
+	.querySelector('.populate-cards')
+	.addEventListener('click', requestNewCards)
+document.querySelector('.delete-cards').addEventListener('click', deleteCards)
 
 const revealCardDescription = (currentCard) => {
-  const existingDescription = document.querySelectorAll(".card-description");
-  existingDescription && existingDescription.forEach((e) => e.remove());
-  const cardDetailsArea = document.querySelector(".card-details-area");
-  const cardDescription = document.createElement("P");
-  const cardStats = document.createElement("P");
-  cardDescription.classList.add("card-description");
-  cardStats.classList.add("card-description");
+	const existingDescription = document.querySelectorAll('.card-description')
+	existingDescription && existingDescription.forEach((e) => e.remove())
+	const cardDetailsArea = document.querySelector('.card-details-area')
+	const cardDescription = document.createElement('P')
+	const cardStats = document.createElement('P')
+	cardDescription.classList.add('card-description')
+	cardStats.classList.add('card-description')
 
-  cardDescription.appendChild(document.createTextNode(currentCard.description));
-  cardStats.appendChild(
-    document.createTextNode(
-      `${currentCard.getRarityIcon} ${currentCard.rarity.toUpperCase()} | HP: ${
-        currentCard.stats.hp
-      } | ATTACK: ${currentCard.stats.attack} | DEFENSE: ${
-        currentCard.stats.defense
-      }`
-    )
-  );
+	cardDescription.appendChild(document.createTextNode(currentCard.description))
+	cardStats.appendChild(
+		document.createTextNode(
+			`${currentCard.getRarityIcon} ${currentCard.rarity.toUpperCase()} | HP: ${
+				currentCard.stats.hp
+			} | ATTACK: ${currentCard.stats.attack} | DEFENSE: ${
+				currentCard.stats.defense
+			}`
+		)
+	)
 
-  cardDetailsArea.appendChild(cardDescription);
-  cardDetailsArea.appendChild(cardStats);
-};
+	cardDetailsArea.appendChild(cardDescription)
+	cardDetailsArea.appendChild(cardStats)
+}
 
 const clearInterface = () => {
-  const existingDescription = document.querySelectorAll(".card-description");
-  existingDescription && existingDescription.forEach((e) => e.remove());
-};
+	const existingDescription = document.querySelectorAll('.card-description')
+	existingDescription && existingDescription.forEach((e) => e.remove())
+}
 
 const statNotify = (e) => {
-  e.target
-    .querySelector(".notification-area")
-    .setAttribute("style", `opacity: 1;`);
+	e.target
+		.querySelector('.notification-area')
+		.setAttribute('style', `opacity: 1;`)
 
-  setTimeout(() => {
-    e.target
-      .querySelector(".notification-area")
-      .setAttribute("style", `opacity: 0;`);
-  }, 700);
-};
+	setTimeout(() => {
+		e.target
+			.querySelector('.notification-area')
+			.setAttribute('style', `opacity: 0;`)
+	}, 700)
+}
 
-const checkServerStatus = () =>{
-  // this doesn't do anything for now
-const serverStatusSpan = document.querySelector('.server-status')
+const checkServerStatus = () => {
+	// this doesn't do anything for now
+	const serverStatusSpan = document.querySelector('.server-status')
 
-serverStatusSpan.innerHTML = "🟢 &nbsp;"
+	serverStatusSpan.innerHTML = '🟢 &nbsp;'
 
-serverStatusSpan.setAttribute("style", "vertical-align: top; font-size: 0.4rem")
-
+	serverStatusSpan.setAttribute(
+		'style',
+		'vertical-align: top; font-size: 0.4rem'
+	)
 }
 
 checkServerStatus()
